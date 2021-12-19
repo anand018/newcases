51392723092259856a35b3214304247e02082396
anand018 <anandpsingh7@gmail.com>
2021-12-19 22:43:48 +0530

    discovery configuration

--- src/main/java/com/covido/newcases/controller/NewCasesController.java
+++ src/main/java/com/covido/newcases/controller/NewCasesController.java
@@ -3,6 +3,7 @@ package com.covido.newcases.controller;
 import com.covido.newcases.dto.StatesDto;
 import com.covido.newcases.services.NewCaseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
